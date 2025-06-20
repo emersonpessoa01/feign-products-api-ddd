9bc12b8856ae1287d74e96737fa6eb60a398f3c6
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-20 09:00:48 -0300

    feat: Updated ProductCategoryResponse :rocket:

--- src/main/java/br/com/raroacademy/feign_products_api_ddd/api/dto/ProductCategoryResponse.java
+++ src/main/java/br/com/raroacademy/feign_products_api_ddd/api/dto/ProductCategoryResponse.java
@@ -1,18 +1,17 @@
 package br.com.raroacademy.feign_products_api_ddd.api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 public class ProductCategoryResponse {
+    // getters e setters
+    @Setter
+    @Getter
     private List<ProductResponseDTO> products;
     private int total;
     private int skip;
     private int limit;
 
-    // getters e setters
-    public List<ProductResponseDTO> getProducts() {
-        return products;
-    }
-    public void setProducts(List<ProductResponseDTO> products) {
-        this.products = products;
-    }
 }
